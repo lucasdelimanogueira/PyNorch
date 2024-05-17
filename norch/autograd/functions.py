fix sum axis autograd and reversed add broadcasted order

norch/autograd/functions.py
@@ -84,9 +84,9 @@ class MatmulBackward:
         x, y = self.input
         
         if x.ndim != y.ndim: # broadcasted case
-            print("KKKKKKKKK", x.ndim, y.ndim)
             aux = (gradient @ y.transpose(-1,-2))
-            return [aux.sum(axis=0), x.transpose(-1,-2) @ gradient]
+            aux_sum = aux.sum(axis=0)
+            return [aux_sum, x.transpose(-1,-2) @ gradient]
         else:
             return [gradient @ y.transpose(-1,-2), x.transpose(-1,-2) @ gradient]
         
@@ -133,14 +133,15 @@ class SumBackward:
 
     def backward(self, gradient):
         input_shape = self.input[0].shape
-        if self.axis is None:
+        if self.axis == -1:
             # If axis is None, sum reduces the tensor to a scalar.
             grad_output = float(gradient.tensor.contents.data[0]) * self.input[0].ones_like()
         else:
             # Broadcast the gradient to the input shape along the specified axis.
-            grad_output_shape = [1 if i in self.axis else dim for i, dim in enumerate(input_shape)]
+            grad_output_shape = list(input_shape)
+            grad_output_shape[self.axis] = 1
             grad_output = gradient.reshape(grad_output_shape)
-            grad_output = grad_output * self.input[0].ones_like()
+            grad_output = grad_output + self.input[0].zeros_like()
         
         return [grad_output]
 class ReshapeBackward:

tests/test_autograd.py
@@ -41,17 +41,19 @@ class TestTensorAutograd(unittest.TestCase):
         norch_tensor1 = norch.Tensor([[[1, 2.5], [3, -4]], [[5, 6], [7, 8]]], requires_grad=True).to(self.device)
         norch_tensor2 = norch.Tensor([[[1, 1.], [1, 1.9]], [[1, 1], [1, 1]]], requires_grad=True).to(self.device)
         norch_result = (norch_tensor1 + norch_tensor2).sum(axis=0).sum(axis=0).sum()
+
         norch_result.backward()
         norch_tensor1_grad = utils.to_torch(norch_tensor1.grad).to(self.device)
         norch_tensor2_grad = utils.to_torch(norch_tensor2.grad).to(self.device)
 
         torch_tensor1 = torch.tensor([[[1, 2.5], [3, -4]], [[5, 6], [7, 8]]], requires_grad=True).to(self.device)
         torch_tensor2 = torch.tensor([[[1, 1.], [1, 1.9]], [[1, 1], [1, 1]]], requires_grad=True).to(self.device)
+        
         torch_result = (torch_tensor1 + torch_tensor2).sum(axis=0).sum(axis=0).sum()
         torch_result.backward()
         torch_tensor1_grad = torch_tensor1.grad
         torch_tensor2_grad = torch_tensor2.grad
-
+        
         self.assertTrue(utils.compare_torch(norch_tensor1_grad, torch_tensor1_grad))
         self.assertTrue(utils.compare_torch(norch_tensor2_grad, torch_tensor2_grad))
 
@@ -70,6 +72,21 @@ class TestTensorAutograd(unittest.TestCase):
         torch_tensor1 = torch.tensor([[[1., 2, 3], [4, 5, 6]]], requires_grad=True).to(self.device)  # Shape (1, 2, 3)
         torch_tensor2 = torch.tensor([1.5, -1, 0], requires_grad=True).to(self.device)  # Shape (3)
         torch_result = (torch_tensor1 + torch_tensor2).sum()
+        torch_result.backward()
+        torch_tensor1_grad = torch_tensor1.grad
+        torch_tensor2_grad = torch_tensor2.grad
+
+        self.assertTrue(utils.compare_torch(norch_tensor1_grad, torch_tensor1_grad))
+        self.assertTrue(utils.compare_torch(norch_tensor2_grad, torch_tensor2_grad))
+
+        ## reversed order broadcasting
+
+        norch_result = (norch_tensor2 + norch_tensor1).sum()
+        norch_result.backward()
+        norch_tensor1_grad = utils.to_torch(norch_tensor1.grad).to(self.device)
+        norch_tensor2_grad = utils.to_torch(norch_tensor2.grad).to(self.device)
+
+        torch_result = (torch_tensor2 + torch_tensor1).sum()
         torch_result.backward()
         torch_tensor1_grad = torch_tensor1.grad
         torch_tensor2_grad = torch_tensor2.grad
@@ -267,7 +284,7 @@ class TestTensorAutograd(unittest.TestCase):
 
     def test_broadcasted_batched_matmul(self):
         """
-        Test autograd from batched matrix multiplication: BxMxP = NxM @ BxMxP
+        Test autograd from broadcasted batched matrix multiplication: BxMxP = NxM @ BxMxP
         """
         B = 3  # Batch size
 

tests/test_operations.py
@@ -37,7 +37,7 @@ class TestTensorOperations(unittest.TestCase):
 
         self.assertTrue(utils.compare_torch(torch_result, torch_expected))
 
-    def test_broadcasting_addition(self):
+    def test_addition_broadcasted(self):
         """
         Test addition of two tensors with broadcasting: tensor1 + tensor2
         """
@@ -50,6 +50,18 @@ class TestTensorOperations(unittest.TestCase):
         torch_tensor2 = torch.tensor([1, 1, 1]).to(self.device)  # Shape (3)
         torch_expected = torch_tensor1 + torch_tensor2
 
+        self.assertTrue(utils.compare_torch(torch_result, torch_expected))
+
+        norch_tensor1 = norch.Tensor([[0, 2]]).to(self.device) 
+        norch_tensor2 = norch.Tensor([[3, 4], [5, -1]]).to(self.device) 
+        norch_result = norch_tensor1 + norch_tensor2
+        torch_result = utils.to_torch(norch_result).to(self.device)
+
+        torch_tensor1 = torch.tensor([[0, 2]]).to(self.device)  
+        torch_tensor2 = torch.tensor([[3, 4], [5, -1]]).to(self.device) 
+        torch_expected = torch_tensor1 + torch_tensor2
+
+        print(torch_result, torch_expected)
         self.assertTrue(utils.compare_torch(torch_result, torch_expected))
 
         norch_result = norch_tensor2 + norch_tensor1
@@ -244,7 +256,7 @@ class TestTensorOperations(unittest.TestCase):
 
     def test_matmul(self):
         """
-        Test batched matrix multiplication: MxP = NxM @ MxP
+        Test matrix multiplication: MxP = NxM @ MxP
         """
         # Creating batched tensors for Norch
         norch_tensor1 = norch.Tensor([[1, 2], [3, -4], [5, 6], [7, 8]]).to(self.device)